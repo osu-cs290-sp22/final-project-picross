c2ccb809b1cdb1075e203caff252422b017ebcba
Andrew Vester <andrewsvester@gmail.com>
2022-06-06 16:44:29 -0700

    Made some 5x5 puzzles and fixed some css to make the side numbers display properly

--- public/numbers.js
+++ public/numbers.js
@@ -9,8 +9,8 @@ templates['numbers'] = template({"1":function(container,depth0,helpers,partials,
     };
 
   return "    <tr>\r\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":4,"column":8},"end":{"line":10,"column":17}}})) != null ? stack1 : "")
-    + "    </tr>\r\n";
+    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":4,"column":8},"end":{"line":12,"column":17}}})) != null ? stack1 : "")
+    + "    </tr>\r\n \r\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -20,17 +20,19 @@ templates['numbers'] = template({"1":function(container,depth0,helpers,partials,
     };
 
   return "            <td>\r\n"
-    + ((stack1 = lookupProperty(helpers,"if").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":12},"end":{"line":8,"column":19}}})) != null ? stack1 : "")
+    + ((stack1 = lookupProperty(helpers,"if").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data,"loc":{"start":{"line":6,"column":12},"end":{"line":10,"column":19}}})) != null ? stack1 : "")
     + "            </td>\r\n";
 },"3":function(container,depth0,helpers,partials,data) {
     return "                "
     + container.escapeExpression(container.lambda(depth0, depth0))
     + "\r\n";
 },"5":function(container,depth0,helpers,partials,data) {
-    return "    <div class = \"side-numbers5x5\">\r\n";
+    return "                &#160\r\n";
 },"7":function(container,depth0,helpers,partials,data) {
-    return "    <div class = \"side-numbers10x10\">\r\n";
+    return "    <div class = \"side-numbers5x5\">\r\n";
 },"9":function(container,depth0,helpers,partials,data) {
+    return "    <div class = \"side-numbers10x10\">\r\n";
+},"11":function(container,depth0,helpers,partials,data) {
     return "        <h4> "
     + container.escapeExpression(container.lambda(depth0, depth0))
     + " </h4>\r\n";
@@ -43,11 +45,11 @@ templates['numbers'] = template({"1":function(container,depth0,helpers,partials,
     };
 
   return "<table>\r\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"topNumbers") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":4},"end":{"line":47,"column":13}}})) != null ? stack1 : "")
+    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"topNumbers") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":4},"end":{"line":15,"column":13}}})) != null ? stack1 : "")
     + "</table>\r\n<div id = \"container\"></div>\r\n\r\n"
-    + ((stack1 = lookupProperty(helpers,"if").call(alias1,(depth0 != null ? lookupProperty(depth0,"x5") : depth0),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.program(7, data, 0),"data":data,"loc":{"start":{"line":51,"column":0},"end":{"line":55,"column":7}}})) != null ? stack1 : "")
+    + ((stack1 = lookupProperty(helpers,"if").call(alias1,(depth0 != null ? lookupProperty(depth0,"x5") : depth0),{"name":"if","hash":{},"fn":container.program(7, data, 0),"inverse":container.program(9, data, 0),"data":data,"loc":{"start":{"line":19,"column":0},"end":{"line":23,"column":7}}})) != null ? stack1 : "")
     + "\r\n\r\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"sideNumbers") : depth0),{"name":"each","hash":{},"fn":container.program(9, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":58,"column":4},"end":{"line":60,"column":13}}})) != null ? stack1 : "")
+    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"sideNumbers") : depth0),{"name":"each","hash":{},"fn":container.program(11, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":26,"column":4},"end":{"line":28,"column":13}}})) != null ? stack1 : "")
     + "</div>";
 },"useData":true});
 })();
